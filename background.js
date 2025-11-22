5d00e8b7a170d1ca7e2557e7604e72459d356c9a
PatriotZest <49161627+PatriotZest@users.noreply.github.com>
2025-11-22 15:33:52 -0600

    referesh

--- background.js
+++
@@ -1,6 +0,0 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'complete' && tab.url) {
-    // Create a new tab with the same URL
-    chrome.tabs.create({ url: tab.url });
-  }
-});
