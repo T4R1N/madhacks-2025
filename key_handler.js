690b66e9b529f790e26f3c167b3041a3ca5bbb5b
PatriotZest <49161627+PatriotZest@users.noreply.github.com>
2025-11-22 16:41:18 -0600

    initial commit

--- key_handler.js
+++ key_handler.js
@@ -1,6 +1,19 @@
-document.addEventListener('keydown', (e) => {
-  if (e.key === 'A') {
-    alert("you pressed A");
-    console.log("you pressed A");
-  }
+//document.addEventListener('keydown', (e) => {
+ // if (e.key === 'A') {
+   // alert("you pressed A");
+   // console.log("you pressed A");
+ // }
+//});
+
+
+import { FishAudioClient } from "fish-audio";
+import { createReadStream } from "fs";
+
+const fishAudio = new FishAudioClient({ apiKey: "bcda3e3389e54ae2bcc965572a75288d" });
+
+const result = await fishAudio.speechToText.convert({
+  audio: createReadStream("welcome.mp3"),
 });
+
+console.log(result.text);
+console.log("Duration (s):", result.duration);
