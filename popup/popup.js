0e2a06943f1b1998b6fa90779a4845f1a828ddcc
Pab200 <pablomillerniharra@gmail.com>
2025-11-22 16:10:25 -0600

    Add files via upload

--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,16 @@
 
 function ButtonClicked() {
-    
+    let button = document.getElementById('toggleButton');
+    let indication = document.getElementById('indicator');
+    let current = button.getAttribute('aria-pressed');
+
+    if (current == 'true') {
+        button.setAttribute('aria-pressed', 'false');
+        indication.textContent= "Simon IS Listening";
+        indication.style.color = "green";
+    } else {
+        button.setAttribute('aria-pressed', 'true');
+        indication.textContent= "Simon is NOT Listening";
+        indication.style.color = "red";
+    }
 }
