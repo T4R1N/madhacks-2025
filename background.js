08ca5c476e1fa141f76fa728138e0ed1ca989be6
Tarin <t4r1n@tuta.io>
2025-11-22 15:13:30 -0600

    key handling

--- background.js
+++
@@ -1,5 +0,0 @@
-self.addEventListener('keydown', (e) => {
-  if (e.key === 'A') {
-    console.log("you pressed A");
-  }
-});
---
+++ key_handler.js
@@ -0,0 +1,6 @@
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'A') {
+    alert("you pressed A");
+    console.log("you pressed A");
+  }
+});
