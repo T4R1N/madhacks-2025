6ec4600407dc2175ef006d433c2c4e04aa676799
PatriotZest <49161627+PatriotZest@users.noreply.github.com>
2025-11-22 17:48:50 -0600

    audio test

--- key_handler.js
+++ key_handler.js
@@ -5,15 +5,56 @@
  // }
 //});
 
+const mic_btn = document.getElementById('speech_button');
+mic_btn.addEventListener("click", recordAudio)
 
-import { FishAudioClient } from "fish-audio";
-import { createReadStream } from "fs";
+let can_record = false;
+let is_recording = false;
 
-const fishAudio = new FishAudioClient({ apiKey: "bcda3e3389e54ae2bcc965572a75288d" });
+let recorder = null;
 
-const result = await fishAudio.speechToText.convert({
-  audio: createReadStream("welcome.mp3"),
-});
+let chunks = [];
+function SetupAudio() {
+  if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices
+      .getUserMedia({
+        audio: true
+      })
+      .then(SetupStream)
+      .catch(err => {
+        console.error(err);
+      });
+  }
+}
+SetupAudio();
 
-console.log(result.text);
-console.log("Duration (s):", result.duration);
+function SetupStream(stream) {
+  recorder = new MediaRecorder(stream);
+
+  recorder.ondataavailable = e => {
+    chunks.push(e.data);
+  }
+  recorder.onstop = e => {
+    const blob = new Blob(chunks, {type: "audio/mp3; codecs=opus"})
+    chunks = [];
+    const audioURL = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = audioURL;
+    a.download = `recording-${Date.now()}.mp3`;
+    a.click();
+  }
+  can_record = true;
+}
+
+function recordAudio() {
+  if(!can_record) return;
+  is_recording = !is_recording;
+  if (is_recording) {
+    recorder.start();
+  } else {
+    recorder.stop();
+  }
+  alert("yes");
+}
+
+
