800169dcbce45400b2a2716f184bc27620b4345a
Tarin <t4r1n@tuta.io>
2025-11-22 18:49:12 -0600

    working fish

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-alert("The test extension is up and running");
+import { FishAudioClient } from "fish-audio";
+import { createReadStream } from "fs";
+import "dotenv/config";
 
+console.log(`Database password is ${ process.env.FISH_API_KEY }`);
 
+const fishAudio = new FishAudioClient({ apiKey: process.env.FISH_API_KEY });
+
+const result = await fishAudio.speechToText.convert({
+  audio: createReadStream("audio.mp3"),
+});
+
+console.log(result.text);
+console.log("Duration (s):", result.duration);
