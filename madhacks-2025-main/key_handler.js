a298302fc163b973d67c07c4fb2e662b7c3942c8
Pab200 <pablomillerniharra@gmail.com>
2025-11-22 18:23:47 -0600

    Add files via upload

--- madhacks-2025-main/key_handler.js
+++ madhacks-2025-main/key_handler.js
@@ -1,6 +1,19 @@
 document.addEventListener('keydown', (e) => {
-  if (e.key === 'A') {
-    alert("you pressed A");
-    console.log("you pressed A");
-  }
+    chrome.storage.local.get('listenKey').then((result) => {
+        if (e.key === result.listenKey) {
+            chrome.storage.local.get('listening', function (result) {
+                const currentValue = result.listening;
+                const newValue = !currentValue; // Toggle the value
+                // Set the new boolean value
+                chrome.storage.local.set({ listening: newValue });
+                if (newValue) {
+                    alert("Simon is listening");
+                }
+                else {
+                    alert("Simon is not listening");
+                }
+            });
+            console.log(`you pressed ${result.listenKey}`);
+        }
+    });
 });
